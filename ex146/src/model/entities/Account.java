6a72ccdcf3616e450a2f621b9b8d07cfe3d5c48a
Otavio Ramos <otavio.omak@gmail.com>
2021-03-06 20:13:32 -0300

    ex146 - starting Account entity

---
+++ ex146/src/model/entities/Account.java
@@ -0,0 +1,26 @@
+package model.entities;
+
+public class Account {
+
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+
+    public Account() {
+    }
+
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public void deposit(Double amount){
+
+    }
+    public void withdraw(Double amount){
+
+    }
+}
